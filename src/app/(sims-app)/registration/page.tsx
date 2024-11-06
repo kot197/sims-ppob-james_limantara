92cab3b8782198e85fcbde20073b39b4b14be40a
kot197 <160315848+kot197@users.noreply.github.com>
2024-11-06 23:54:23 +0700

    Logo /registration

--- src/app/(sims-app)/registration/page.tsx
+++ src/app/(sims-app)/registration/page.tsx
@@ -7,7 +7,7 @@ export default function Registration() {
         <div className="flex-1 justify-items-center content-center">
             <div className="flex flex-col items-center w-3/5">
                 <div className="p-7 flex gap-2">
-                    <Image src='/logo.png' width={32} height={32} alt="Logo"/>
+                    <Image src='/Logo.png' width={32} height={32} alt="Logo"/>
                     <span className="text-xl font-semibold">SIMS PPOB</span>
                 </div>
                 <p className="text-xl font-semibold">Lengkapi data untuk membuat akun</p>
